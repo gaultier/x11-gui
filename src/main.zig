298ffde87fdfab2223035de33c959c598a2d2a90
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-06-11 22:14:47 +0200

    [wip]

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,54 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn handshake(stream: *std.net.Stream) !void {
+    const authorization = "MIT-MAGIC-COOKIE-1";
+    const Request = packed struct {
+        endianness: u8 = 'l',
+        pad1: u8 = 0,
+        major_version: u16 = 11,
+        minor_version: u16 = 0,
+        authorization_len: u16 = authorization.len,
+        authorization_data_len: u16 = 16,
+        pad2: u16 = 0,
+    };
+    comptime std.debug.assert(@sizeOf(Request) == 12);
+    var req = Request{};
+    try stream.writeAll(std.mem.asBytes(&req));
+    try stream.writeAll(std.mem.asBytes(&authorization));
+    try stream.writeAll(std.mem.asBytes(&[2]u8{ 0, 0 }));
+    const cookie = [16]u8{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    try stream.writeAll(std.mem.asBytes(&cookie));
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const Response = packed struct {
+        success: bool,
+        pad1: u8,
+        major_version: u16,
+        minor_version: u16,
+        length: u16,
+        release_number: u32,
+        resource_id_base: u32,
+        resource_id_mask: u32,
+        motion_buffer_size: u32,
+        vendor_length: u16,
+        maximum_request_length: u16,
+        screens_in_root_count: u8,
+        formats_count: u8,
+        image_byte_order: u8,
+        bitmap_format_bit_order: u8,
+        bitmap_format_scanline_unit: u8,
+        bitmap_format_scanline_pad: u8,
+        min_keycode: u8,
+        max_keycode: u8,
+        pad2: u32,
+    };
+    var read_buffer = [_]u8{0} ** (1 << 15);
+    const n_read = try stream.readAtLeast(&read_buffer, @sizeOf(Response));
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    const response: Response = std.mem.bytesToValue(Response, read_buffer[0..n_read]);
+    std.debug.print("{}", .{response});
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    var socket = try std.net.connectUnixSocket("/tmp/.X11-unix/X0");
+    try handshake(&socket);
 }
